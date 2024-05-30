189653d7ca9f6d96aee198182205b0b978c64c64
= <=>
2024-05-30 13:10:47 -0300

    update

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -6,6 +6,11 @@ const Buscador = ({ setInputFilter }) => {
 
   return (
     <>
+      <img
+        src="./public/MARCA_JOSENUCETEHIJOS_2019-3.jpg"
+        alt=""
+        style={{ height: "100px", width: "100px" }}
+      />
       <h2>BUSCADOR DE PROVEEDORES</h2>
       <label htmlFor="buscador">Ingrese el proveedor</label>
       <input type="text" name="buscador" id="buscador" onChange={handleInput} />
