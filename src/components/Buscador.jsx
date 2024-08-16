2e49338e194540c86069b72777422a4add5a71f3
= <=>
2024-08-16 12:24:06 -0300

    focus en el buscador

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,28 +1,25 @@
 const Buscador = ({ setInputFilter }) => {
   const handleInput = (e) => {
-    const inputProveedor = e.target.value;
-    setInputFilter(inputProveedor);
-  };
+    useEffect(() => {
+      document.getElementById('buscador').focus()
+    }, [])
+    const inputProveedor = e.target.value
+    setInputFilter(inputProveedor)
+  }
 
   return (
     <>
       <img
         src="./MARCA_JOSENUCETEHIJOS_2019-3.jpg"
         alt=""
-        style={{ height: "100px", width: "100px" }}
+        style={{ height: '100px', width: '100px' }}
       />
       <h2>BUSCADOR DE PROVEEDORES</h2>
       <label htmlFor="buscador">Ingrese el proveedor</label>
-      <input
-        type="text"
-        name="buscador"
-        id="buscador"
-        onChange={handleInput}
-        autocomplete="off"
-      />
+      <input type="text" name="buscador" id="buscador" onChange={handleInput} autocomplete="off" />
       <br />
       <br />
     </>
-  );
-};
-export default Buscador;
+  )
+}
+export default Buscador
