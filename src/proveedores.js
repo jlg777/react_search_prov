b6bc9758303951c3b1f43b679263e8de9b7e651e
= <=>
2024-10-28 12:07:24 -0300

    gas meridional

--- src/proveedores.js
+++ src/proveedores.js
@@ -151,7 +151,7 @@ const proveedores = [
   { nombre: 'FERNANDEZ JOSE RICARDO', correo: 'ventas@rfmayorista.com.ar' },
   { nombre: 'FLEX PIPE S.A', correo: 'cobranzas.wae@gmail.com' },
   { nombre: 'GARRO LUIS ARNALDO', correo: 'electrovilla@hotmail.com.ar' },
-  { nombre: 'GAS MERIDIONAL', correo: 'csm@gasmeridional.com.ar' },
+  { nombre: 'GAS MERIDIONAL', correo: 'cobranzas@gasmeridional.com.ar' },
   { nombre: 'GILIGROUP SAS', correo: 'cobranza@saavedrasi.com.ar' },
   {
     nombre: 'GODOY ALEJANDRA FRANCISCA Y MARCO ADOLFO',
